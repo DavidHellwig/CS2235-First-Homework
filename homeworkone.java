5e2194345b3579db2776ae6542c8195a6a378af4
IAMACSStudent <74218656+IAMACSStudent@users.noreply.github.com>
2021-03-24 12:10:56 -0600

    Add files via upload

---
+++ homeworkone.java
@@ -0,0 +1,22 @@
+//David Hellwig
+//Assignment 1
+//CS 2235
+//Due Date 1-21-2021
+
+public class homeworkone {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is David Hellwig");
+        String food = "Pizza";
+        System.out.printf("My favorite food is %s", food);
+        System.out.println("");
+        int grad = 2025;
+        System.out.printf("I hope to graduate by %s , but hopefully sooner", grad);
+        System.out.println("");
+        double height;
+        height = 5.91;
+        System.out.printf("My height is %s feet", height);
+
+    }
+}
+
+
